d4de3c77897bf5c452df8d93e7fd68fed9087f2d
walterkamoyo <waltermoyo89@gmail.com>
2022-09-28 21:55:04 +0100

    Initial commit

---
+++ Assets/Scripts/GroundPiece.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundPiece : MonoBehaviour
+{
+    public bool isColored = false;
+
+    public void Colored(Color color)
+    {
+        GetComponent<MeshRenderer>().material.color = color;
+        isColored = true;
+
+        FindObjectOfType<GameManager>().CheckComplete();
+    }
+
+}
+
